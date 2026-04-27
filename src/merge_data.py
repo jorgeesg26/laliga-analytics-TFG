fb3ef24aff256faf9fadbf2f11f6cb9bed5e1e24
jorgeesg26 <jsalaba2610@g.educaand.es>
2026-04-27 19:29:14 +0200

    csv definitivo creado
    he mezclasdo los 5 csv en uno para solo usar un archivo de csv en el proyecto

---
+++ src/merge_data.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+files = [
+    "data/2020-2021.csv",
+    "data/2021-2022.csv",
+    "data/2022-2023.csv",
+    "data/2023-2024.csv"
+]
+
+dfs = []
+
+for file in files:
+    df = pd.read_csv(file, encoding="latin1")
+
+    # Seleccionamos columnas importantes
+    df = df[[
+        "Date", "HomeTeam", "AwayTeam",
+        "FTHG", "FTAG",
+        "HS", "AS", "HST", "AST",
+        "HY", "AY"
+    ]]
+
+    # Renombramos
+    df.columns = [
+        "fecha", "local", "visitante",
+        "goles_local", "goles_visitante",
+        "tiros_local", "tiros_visitante",
+        "tiros_puerta_local", "tiros_puerta_visitante",
+        "amarillas_local", "amarillas_visitante"
+    ]
+
+    temporada = file.split("/")[-1].replace(".csv", "")
+    df["temporada"] = temporada
+
+    dfs.append(df)
+
+final_df = pd.concat(dfs)
+
+final_df.to_csv("data/laliga_clean.csv", index=False)
+
+print("Dataset limpio listo ✅")
